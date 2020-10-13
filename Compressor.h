dbf11b4cbc3d5b74f8901e5d285f4bf3581cabb6
Low Wave Studios <68336655+lowwavestudios@users.noreply.github.com>
2020-10-13 17:40:27 +0100

    Add files via upload

---
+++ Compressor.h
@@ -0,0 +1,276 @@
+#pragma once
+
+//#include "AudioDevEffect.h"
+#include "EnvelopeShaper.h"
+
+#include <cmath>
+#include <algorithm>
+
+class Compressor //: public AudioDevEffect
+{
+public:
+    // callback for initializing with samplerate
+    void prepareForPlayback(float samplerate, int numSamp, int numChannels) //override
+    {
+        m_EnvelopeShaper.prepareForPlayback(samplerate);
+        HPF.coefficients = dsp::IIR::Coefficients<float>::makeHighPass(samplerate, 20.f, 1.f);
+        LPF.coefficients = dsp::IIR::Coefficients<float>::makeLowPass(samplerate, 20000.f, 1.f);
+
+        sideChain.setSize(1, numSamp, false, false, false);
+
+        numCh = numChannels;
+        numS = numSamp;
+    }
+    
+    void processBuffer(AudioSampleBuffer &inputBuffer, AudioSampleBuffer &originalBuffer, AudioSampleBuffer &highpassedBuffer, AudioSampleBuffer &bufferForVisual, bool lookahead, bool sc)
+    {
+        auto w = sideChain.getWritePointer(0);
+
+        auto numsamples = lookahead == true ? originalBuffer.getNumSamples() : inputBuffer.getNumSamples();
+        //auto numsamples = inputBuffer.getNumSamples();
+
+        if (numCh > 1)
+        {
+            for (int i = 0; i < numsamples; i++)
+            {
+                float L, R;
+                if (lookahead == true)
+                {
+                    int ch;
+                    if (sc == true)
+                    {
+                        ch = 2;
+                    }
+                    else { ch = 0; }
+
+                    L = originalBuffer.getSample(ch, i);
+                    R = originalBuffer.getSample(ch + 1, i);
+                }
+                else
+                {
+                    int ch;
+                    if (sc == true)
+                    {
+                        ch = 2;
+                    }
+                    else { ch = 0; }
+
+                    L = inputBuffer.getSample(ch, i);
+                    R = inputBuffer.getSample(ch + 1, i);
+                }
+                
+
+                float detectionSignal;
+                if (fabs(R) == fmaxf(fabs(L), fabs(R)))
+                { 
+                    detectionSignal = R;
+                }
+                else { detectionSignal = L; }
+         
+                //filter sidechain
+                detectionSignal = HPF.processSample(detectionSignal);
+                detectionSignal = LPF.processSample(detectionSignal);
+
+                
+                // rectify detection signal
+                //detectionSignal = fabs(detectionSignal);
+                //detectionSignal *= -1.f;
+
+                // apply envelope shaping to detection signal
+                m_EnvelopeShaper.processAudioSample(detectionSignal);
+
+                // check if the detection signal exceeds the threshold
+                detectionSignal = amplitudeToDecibel(detectionSignal);
+
+                w[i] = detectionSignal;
+
+
+            }
+        }
+        else 
+        {
+            for (int i = 0; i < numsamples; i++)
+            {
+                float detectionSignal;
+                if (lookahead == true)
+                {
+                    int ch;
+                    if (sc == true)
+                    {
+                        ch = 1;
+                    }
+                    else { ch = 0; }
+
+                    detectionSignal = originalBuffer.getSample(ch, i);
+                }
+                else
+                {
+                    int ch;
+                    if (sc == true)
+                    {
+                        ch = 1;
+                    }
+                    else { ch = 0; }
+
+                    detectionSignal = inputBuffer.getSample(ch, i);
+                }
+
+                //filter sidechain
+                detectionSignal = HPF.processSample(detectionSignal);
+                detectionSignal = LPF.processSample(detectionSignal);
+
+
+                // rectify detection signal
+                //detectionSignal = fabs(detectionSignal);
+                //detectionSignal *= -1.f;
+
+                // apply envelope shaping to detection signal
+                m_EnvelopeShaper.processAudioSample(detectionSignal);
+
+                // check if the detection signal exceeds the threshold
+                detectionSignal = amplitudeToDecibel(detectionSignal);
+
+                w[i] = detectionSignal;
+
+            }
+        }
+        
+
+        for (int ch = 0; ch < numCh && ch < 2; ch++)
+        {
+            float* input = highpassedBuffer.getWritePointer(ch);
+            float* inputV = bufferForVisual.getWritePointer(ch);
+            for (int i = 0; i < numsamples; i++)
+            {
+                auto sidechain = sideChain.getSample(0, i);
+                
+                //sc = amplitudeToDecibel(sc);
+
+                if (sidechain > m_Threshold)
+                {
+                    // compute required gain reduction
+                    float scale = 1.f - (1.f / m_Ratio);
+                    float gain = scale * (m_Threshold - sidechain);
+
+                    
+                    gain = decibelToAmplitude(gain);
+                    tick = gain;
+
+                    input[i] *= gain;
+                    inputV[i] *= gain;
+           
+                }
+            }
+        }
+
+        //sideChain.clear();
+        
+
+        
+    }
+    // callback for audio processing
+    void processAudioSample(float & sample) //override
+    {
+        // copy signal in detection signal
+        float detectionSignal = sample;
+
+        //filter sidechain
+        detectionSignal = HPF.processSample(detectionSignal);
+        detectionSignal = LPF.processSample(detectionSignal);
+
+        
+        // rectify detection signal
+        detectionSignal = fabs(detectionSignal);
+        
+        // apply envelope shaping to detection signal
+        m_EnvelopeShaper.processAudioSample(detectionSignal);
+        
+        // check if the detection signal exceeds the threshold
+        detectionSignal = amplitudeToDecibel(detectionSignal);
+        if (detectionSignal > m_Threshold)
+        {
+            // compute required gain reduction
+            float scale = 1.f - (1.f / m_Ratio);
+            float gain = scale * (m_Threshold - detectionSignal);
+
+            gain = decibelToAmplitude(gain);
+
+            tick = gain; //get tick from Compressor
+
+            // apply gain reduction
+            sample *= gain;
+        }
+    }
+    
+    float getTick()
+    {
+        
+        return tick;
+    }
+    void setSC(float hpf, float lpf, double samplerate)
+    {
+        HPF.snapToZero();
+        LPF.snapToZero();
+
+        HPF.coefficients = dsp::IIR::Coefficients<float>::makeHighPass(samplerate, hpf, 1.f);
+        LPF.coefficients = dsp::IIR::Coefficients<float>::makeLowPass(samplerate, lpf, 1.f);
+
+
+    }
+    
+    // setters for compression parameters
+    void setThreshold(float threshold)
+    {
+        m_Threshold = threshold;
+    }
+    
+    void setRatio(float ratio)
+    {
+        m_Ratio = ratio;
+    }
+    
+    void setAttack(float attack)
+    {
+        m_EnvelopeShaper.setAttack(attack);
+    }
+    
+    void setRelease(float release)
+    {
+        m_EnvelopeShaper.setRelease(release);
+    }
+    void setHold(float hold)
+    {
+        m_EnvelopeShaper.setHold(hold);
+    }
+
+private:
+    // compressor private variables
+    float m_Threshold;
+    float m_Ratio;
+    float tick = 1.f;
+    dsp::IIR::Filter<float> HPF;
+    dsp::IIR::Filter<float> LPF;
+    dsp::IIR::Coefficients<float> hpfCoeffs;
+    dsp::IIR::Coefficients<float> lpfCoeffs;
+    
+    EnvelopeShaper m_EnvelopeShaper;
+
+    AudioSampleBuffer sideChain;
+    int numS, numCh;
+    
+    const float BOUND_LOG = -96.f;
+    const float BOUND_LIN = decibelToAmplitude(BOUND_LOG);
+    
+    // transform amplitude to decibel
+    float amplitudeToDecibel(float amplitude)
+    {
+        amplitude = std::max(amplitude, BOUND_LIN);
+        return 20.f * log10(amplitude);
+    }
+    
+    // transform decibel to amplitude
+    float decibelToAmplitude(float decibel)
+    {
+        return pow(10.f, decibel / 20.f);
+    }
+}; 
---
+++ PluginProcessor.h
@@ -0,0 +1,253 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#ifndef PLUGINPROCESSOR_H_INCLUDED
+#define PLUGINPROCESSOR_H_INCLUDED
+
+#define CROSSOVER_ID "crossover"
+#define CROSSOVER_NAME "Crossover"
+#define SCHIPASS_ID "side-chain HPF"
+#define SCHIPASS_NAME "Side-Chain HPF"
+#define SCLOPASS_ID "side-chain LPF"
+#define SCLOPASS_NAME "Side-Chain LPF"
+
+#define THRESHOLD_ID "threshold high-freq"
+#define THRESHOLD_NAME "Threshold High-freq"
+#define THRESHOLD2_ID "threshold low-freq"
+#define THRESHOLD2_NAME "Threshold Low-freq"
+
+#define ATTACK_ID "attack high-freq"
+#define ATTACK_NAME "Attack High-freq"
+#define RELEASE_ID "release high-freq"
+#define RELEASE_NAME "Release High-freq"
+#define HOLD_ID "hold high-freq"
+#define HOLD_NAME "Hold High-freq"
+
+#define ATTACK2_ID "attack low-freq"
+#define ATTACK2_NAME "Attack Low-freq"
+#define RELEASE2_ID "release low-freq"
+#define RELEASE2_NAME "Release Low-freq"
+#define HOLD2_ID "hold low-freq"
+#define HOLD2_NAME "Hold Low-freq"
+
+#define MIX_ID "dry/wet"
+#define MIX_NAME "Dry/Wet"
+
+#define INPUT_ID "input"
+#define INPUT_NAME "Input Gain"
+#define OUTPUT_ID "output"
+#define OUTPUT_NAME "Output Gain"
+#define OS_ID "oversample"
+#define OS_NAME "Oversample"
+#define WET_ID "dry/wet"
+#define WET_NAME "Dry/Wet"
+#define LA_ID "lookahead"
+#define LA_NAME "Lookahead"
+
+#define SC_ID "sidechain"
+#define SC_NAME "Side Chain"
+
+#define SOLOHI_ID "solo hifreq"
+#define SOLOHI_NAME "Solo High-freq."
+#define SOLOLOW_ID "solo lofreq"
+#define SOLOLOW_NAME "Solo Low-freq."
+
+#include <JuceHeader.h>
+#include "AudioDevEffect.h"
+#include "Compressor.h"
+#include "EnvelopeShaper.h"
+#include "Compressor2.h"
+#include "EnvelopeShaper2.h"
+#include "EnvelopeShaperVisual.h"
+#include "EnvelopeShaperVisualLow.h"
+#include "DSPFilters4Juce/DSPFilters.h"
+
+
+//==============================================================================
+/**
+*/
+//create Visualiser class
+class Visualiser : public AudioVisualiserComponent
+{
+public:
+    Visualiser() : AudioVisualiserComponent(1)
+    {
+        setBufferSize(512);
+        setSamplesPerBlock(128);
+        setColours(Colours::transparentBlack, Colour::fromRGBA(7, 165, 18, 100));
+        setOpaque(false);
+        setRepaintRate(60);
+        
+    }
+
+    //override paintChannel method in AudioVisualiserComponent
+    void Visualiser::paintChannel(Graphics& g, Rectangle<float> area,
+        const Range<float>* levels, int numLevels, int nextSample)
+    {
+        Path p;
+        //create a stroke
+        auto visualStrokePath = PathStrokeType(3.f, juce::PathStrokeType::JointStyle::curved, juce::PathStrokeType::EndCapStyle::rounded);
+
+        getChannelAsPath(p, levels, numLevels, nextSample);
+
+        g.strokePath(p, visualStrokePath, AffineTransform::fromTargetPoints(0.0f, -1.0f, area.getX(), area.getY(),
+            0.0f, 1.0f, area.getX(), area.getBottom(),
+            (float)numLevels, -1.0f, area.getRight(), area.getY()));
+    }
+};
+
+class VisualiserLow : public AudioVisualiserComponent
+{
+public:
+    VisualiserLow() : AudioVisualiserComponent(1)
+    {
+        setBufferSize(512);
+        setSamplesPerBlock(128);
+        setColours(Colours::transparentBlack, Colour::fromRGBA(7, 112, 165, 100));
+        setOpaque(false);
+        setRepaintRate(60);
+
+    }
+
+    //override paintChannel method in AudioVisualiserComponent
+    void VisualiserLow::paintChannel(Graphics& g, Rectangle<float> area,
+        const Range<float>* levels, int numLevels, int nextSample)
+    {
+        Path p;
+        //create a stroke
+        auto visualStrokePath = PathStrokeType(3.f, juce::PathStrokeType::JointStyle::beveled, juce::PathStrokeType::EndCapStyle::rounded);
+
+        getChannelAsPath(p, levels, numLevels, nextSample);
+
+        g.strokePath(p, visualStrokePath, AffineTransform::fromTargetPoints(0.0f, -1.0f, area.getX(), area.getY(),
+            0.0f, 1.0f, area.getX(), area.getBottom(),
+            (float)numLevels, -1.0f, area.getRight(), area.getY()));
+    }
+};
+
+class IceyGateAudioProcessor  : public AudioProcessor
+{
+public:
+    //==============================================================================
+    IceyGateAudioProcessor();
+    ~IceyGateAudioProcessor();
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (AudioBuffer<float>&, MidiBuffer&) override;
+
+    //==============================================================================
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    
+    //const String getInputChannelName(int channelIndex) const override;
+    //const String getOutputChannelName(int channelIndex) const override;
+    //bool isInputChannelStereoPair(int index) const override;
+    //bool isOutputChannelStereoPair(int index) const override;
+
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const String getProgramName (int index) override;
+    void changeProgramName (int index, const String& newName) override;
+
+
+    //==============================================================================
+    void getStateInformation (MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    void prepareSignalVisualiser(AudioBuffer<float> newbuffer);
+    void prepareSignalVisualiserLowFreq(AudioBuffer<float> newbuffer);
+    void updateFilter();
+
+    
+    
+    AudioProcessorValueTreeState parameters;
+    AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+
+    Compressor compressor;
+    Compressor2 compressorLowFreq;
+
+    float crossoverFreq, scHPF, scLPF;
+    int lastSampleRate {44100};
+
+    int curRatio;
+    float curThreshHigh, curAtkTimeHigh, curRelTimeHigh, curHoldTimeHigh;
+    float curThreshLow, curAtkTimeLow, curRelTimeLow, curHoldTimeLow;
+
+    float inputGain, outputGain;
+
+    float previousInputGain;
+    float previousOutputGain, previousDrywet;
+    bool lookahead;
+    int delayedBufferLength;
+    int writePosition { 0 };
+    float drywet;
+    bool muteInit { true }; //bool for initial mute of output
+    bool muteLA { true };
+    bool sidechain;
+    bool muteSC{ true };
+    bool soloHi{ false }, soloLow{ false };
+    int numChanOriginal{2}, numChanNew;
+
+
+    
+    LinearSmoothedValue<float> freqSmooth;
+    
+    void IceyGateAudioProcessor::fillDelayedBuffer(int ch, int bufferNumSamples, const int delayedBufferLength, const float* bufferData, const float* delayedBufferData);
+    void IceyGateAudioProcessor::getFromDelayBuffer(AudioBuffer<float>& buffer, int ch, const int bufferNumSamples, const int delayBufferLength, const float* bufferData, const float* delayedBufferData);
+
+    Visualiser visualiser;
+    VisualiserLow visualiserLowFreq;
+
+    EnvelopeShaperVisual visualiserShaper;
+    EnvelopeShaperVisualLow visualiserShaperLowFreq;
+    
+private:
+
+    void IceyGateAudioProcessor::setParams();
+
+    AudioSampleBuffer delayedBuffer;
+    AudioSampleBuffer lateBuffer;
+    AudioSampleBuffer originalBuffer;
+    AudioSampleBuffer hiBufferVisual;
+    AudioSampleBuffer lowBufferVisual;
+
+    //DSP::Butterworth<DSP::HIGHPASS, 1> hiPassFilter;
+    dsp::ProcessorDuplicator<dsp::StateVariableFilter::Filter<float>, dsp::StateVariableFilter::Parameters<float>> hiPassFilter;
+    dsp::ProcessorDuplicator<dsp::StateVariableFilter::Filter<float>, dsp::StateVariableFilter::Parameters<float>> hiPassFilter2;
+
+    AudioSampleBuffer dryBuffer;
+    AudioSampleBuffer wetBufferLowFreq, wetBufferHighFreq;
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(IceyGateAudioProcessor)
+
+};
+
+#endif  // PLUGINPROCESSOR_H_INCLUDED
